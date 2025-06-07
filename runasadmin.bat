90f904a1b6a575ebdab09dddc0c4baee04df3639
xqrto <janfelixlindner@gmail.com>
2025-06-07 14:54:24 +0200

    Delete runasadmin.bat

--- runasadmin.bat
+++
@@ -1,9 +0,0 @@
-
-@echo off
-if "%1"=="--help" goto help
-powershell -Command "Start-Process '%~2' -Verb runAs"
-goto end
-:help
- echo Usage: runasadmin.bat --run <file>
- echo Starts a file with admin rights.
-:end
